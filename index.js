ae6af61bede028861138ee4ea3e2e36465175316
kulani <h1l1zatkh@gmail.com>
2023-05-16 13:01:22 +0200

    my portfolio project

---
+++ index.js
@@ -0,0 +1,30 @@
+/* 
+1. Tech
+Chrome
+VS-code
+Git & GitHub
+
+2. Design - Figma
+UX/UI wireframe, prototype & design 
+
+3. Programming lang
+HTML 5 - responsive web, accesibility 
+CSS 5 - tailwindCSS
+JavaScript & Java/Python
+DOM
+JSON
+API
+
+4. Framework & Libraries
+Figma
+React.Js/VueJs
+Node.Js
+Express.Js
+
+5. Database
+MangoDB
+
+*/
+
+
+
